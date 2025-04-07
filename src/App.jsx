592dd99d7e64f4f2940a9683a25e936fe886e0ac
Akash <hafejmohammadakash@gmail.com>
2025-04-07 12:54:39 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h2>React Core Concept Recap</h2>
+      <ol>
+        <li>Components</li>
+        <li>JSX</li>
+        <li>Props</li>
+        <li>Event Handler</li>
+        <li>State</li>
+        <li>load Data</li>
+      </ol>
+      <hr />
+
+      
+    </>
+  )
+}
+
+export default App
